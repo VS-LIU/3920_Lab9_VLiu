82b6439fc4c25b9ab5c7b4cd4f22bced68809343
VS-LIU <vctl95@gmail.com>
2024-03-24 20:48:02 -0700

    Fix typo

--- databaseConnectionSequelize.js
+++ databaseConnectionSequelize.js
@@ -1,7 +1,7 @@
 require('dotenv').config(); 
 
 const dbConfigLocal = 
-`mysql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.en v.DB_HOST}/${process.env.DB_NAME}`; 
+`mysql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}`; 
 
 var databaseConnectionString = dbConfigLocal; 
 
