e5042ef292219e85c8798a514f947025c609050b
VS-LIU <vctl95@gmail.com>
2024-03-24 21:17:22 -0700

    Implement pet_type model

---
+++ models/pet_type.js
@@ -0,0 +1,23 @@
+const {Sequelize, DataTypes} = require('sequelize'); 
+const databaseConnectionString = include('/databaseConnectionSequelize'); 
+const sequelize = new Sequelize(databaseConnectionString); 
+
+const petTypeModel = sequelize.define('pet_type', {  // This line creates the model for the pet table.  
+    pet_type_id: {
+        type: Sequelize.INTEGER, 
+        autoIncrement: true, 
+        primaryKey: true, 
+        allowNull: false
+    },  
+    type: {
+        type: Sequelize.STRING, 
+        allowNull: false,
+        unique: true }
+    }, 
+    {tableName: 'pet_type',  
+     timestamps: false, 
+     singular: 'pet_type', 
+     plural: 'pet_type'} 
+); 
+
+module.exports = petTypeModel;  
