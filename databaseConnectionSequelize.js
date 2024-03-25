6dad93123e2e47cfca56f3f7b6c9f004ab43d6fa
VS-LIU <vctl95@gmail.com>
2024-03-24 22:48:14 -0700

    Add encodeURIcomponent for special characters

--- databaseConnectionSequelize.js
+++ databaseConnectionSequelize.js
@@ -1,8 +1,9 @@
 require('dotenv').config(); 
 
-const dbConfigLocal = 
-`mysql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}`; 
+// const dbConfigLocal = 
+// `mysql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`; 
 
+const dbConfigLocal = `mysql://${encodeURIComponent(process.env.DB_USER)}:${encodeURIComponent(process.env.DB_PASSWORD)}@${encodeURIComponent(process.env.DB_HOST)}:${encodeURIComponent(process.env.DB_PORT)}/${encodeURIComponent(process.env.DB_NAME)}`;
 var databaseConnectionString = dbConfigLocal; 
 
 module.exports = databaseConnectionString; 
